41a74c2c096cd78a335f9a96888cbc8f083c5f45
Jason Watkins <jasonwatkins@hotmail.com>
2020-07-18 07:44:43 -0400

    Update RobocopyVMstoE.cmd
    A simple robocopy script to backup data from one directory to another. Great for scheduled tasks...

--- RobocopyVMstoE.cmd
+++ RobocopyVMstoE.cmd
@@ -1,3 +1,4 @@
 @echo off
-robocopy "D:\VMs" "E:\VM_Backups" /MIR /FFT /Z /XA:H /W:5
+REM Simple robocopy script to backup data from one directory to another. Great for scheduled tasks...
+robocopy "C:\Source_Data" "E:\Backup_Directory" /MIR /FFT /Z /XA:H /W:5
 exit
