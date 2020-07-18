50ea2104758d028a734b19f8b337efee3eab057a
Jason Watkins <jasonwatkins@hotmail.com>
2020-07-18 07:38:28 -0400

    Update lite_rsync.sh
    A simple rsync script to backup a user profile from a macOS to an SMB share on another computer.

--- lite_rsync.sh
+++ lite_rsync.sh
@@ -1,13 +1,13 @@
 LOG=~/rsync.log
-LDOC=/Users/jasonrw/Documents
-LPIC=/Users/jasonrw/Pictures
-LCL=/Users/jasonrw/
-SRV=/Volumes/jasonwatkins/
+#LDOC=/Users/username/Documents Not needed
+#LPIC=/Users/username/Pictures Not needed
+LCL=/Users/username/
+SRV=/Volumes/username/
 # Check to see if server share is mounted
 # From: https://www.cyberciti.biz/faq/howto-check-if-a-directory-exists-in-a-bash-shellscript/
 if [ ! -d "$SRV" ]
 then
-  open 'smb://jasonwatkins:*@192.168.1.5/jasonwatkins'
+  open 'smb://username:*@192.168.1.2/uswrname'
   # https://apple.stackexchange.com/questions/697/how-can-i-mount-an-smb-share-from-the-command-line
 else
   echo "Server share already mounted" >> $LOG # From: https://www.tecmint.com/echo-command-in-linux/
